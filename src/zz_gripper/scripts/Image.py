f292d6cae0abd37f69c002284b3f405ad5402bd8
Desiree Santos <Desiree_Santos@gap.com>
2017-12-13 04:58:08 -0200

    Implements the logic to deal with mask and discover object

--- src/zz_gripper/scripts/Image.py
+++ src/zz_gripper/scripts/Image.py
@@ -5,6 +5,10 @@ from modules.Color import Color
 from cv_bridge import CvBridge, CvBridgeError
 
 class image():
+
+	""" ROS SUBSCRIPTIONS """
+	self.cmd_vel_pub = rospy.Publisher("cmd_vel", Twist, queue_size=5)
+
 	
 	""" VARIABLES """
 	def __init__(self):
@@ -28,7 +32,31 @@ class image():
 		self._latestMaskedImage = cv2.inRange(hsv, lower, upper) 
 		return self._latestMaskedImage 
 
-	def find_object():
-		
-		return 	
+	def find_object(self):
+
+		# From mask data, find the center point 
+		h, w, d = self._latestMaskedImage.shape
+    search_top = 3*h/4
+    search_bot = 3*h/4 + 20
+    mask[0:search_top, 0:w] = 0	
+    mask[search_top:h, 0:w] = 0
+    M = cv2.moments(mask)
+
+    if M['m00'] > 0:
+ 			cx = int(M['m10']/M['m00'])
+			cy = int(M['m01']/M['m00'])
+			cv2.circle(image,(cx,cy),20,(0,0,255),-1)
+			err = cx - w/2
+
+      self.twist.linear.x = 0.5
+      self.twist.angular.z = -float(err) / 100
+      self.cmd_vel_pub.publish(self.twist)
+
+     # Find yellow circle 
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+		img = cv2.medianBlur(gray,5)
+		cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+		circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20, param1=50,param2=30,minRadius=0,maxRadius=0)    
+	
+		return  True if len(circle) >= 1 else False 	
 		
